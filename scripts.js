db5d9c81c0e54b89fd427b056ee48c7134490349
Jay Chen <jaychenjjc@gmal.com>
2023-06-15 15:26:39 -0400

    2 and 3

--- scripts.js
+++ scripts.js
@@ -12,11 +12,50 @@ function print(num){
     console.log(num);
 }
 
-myEach(print,Array);
+//myEach(print,Array);
 
 
 
 // map() function
+
+function myMap(callbackfunc, ary){
+let newAry = [];
+for(let i = 0; i<ary.length; i++){
+    newAry.push(callbackfunc(ary[i]));
+}
+
+return newAry;
+};
+
+function add1(num){
+   return num*2;
+};
+
+let newArray = myMap(add1, Array);
+
+//console.log(newArray);
+
+
+//filter() function
+function filter(callbackfunc, ary){
+    let newAry = []
+    for(let i = 0; i<ary.length; i++){
+        if(callbackfunc(ary[i])){
+            newAry.push(ary[i]);
+        }
+    }
+    return newAry;
+}
+
+function has3letters(string){
+    return string.length===3;
+}
+let stringAry = ['Jacob', "bob", "sam", "johnathan"];
+
+newArray = filter(has3letters, stringAry);
+console.log(newArray)
+
+
 
 
 
